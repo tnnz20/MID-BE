94ecea1d5bae11bc1041002424ee6de315870d15
tnnz20 <kaitora200@gmail.com>
2023-07-27 17:22:24 +0800

    feat: add video and comment route

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import 'dotenv/config'
+
 import mongoose from 'mongoose';
 import express from 'express';
-import ProductRouter from './routes/ProductRouter.js';
+
 import CustomError from "./utils/CustomError.js"
 import ErrorController from './controllers/ErrorController.js';
 
+import ProductRouter from './routes/ProductRouter.js';
+import VideoRouter from './routes/VideoRouter.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 const DB_URL = process.env.DATABASE_URL
-
 
 app.use(express.json())
 
@@ -19,13 +21,18 @@ const db = mongoose.connection
 db.on('error', err => console.log(err))
 db.once('connected', ()=> console.log('Database Connected'))
 
+
+// Router
+app.use('/', VideoRouter)
 app.use('/product', ProductRouter)
+app.use('/comment', CommentRouter)
 
 app.use('*', (req, _, next)=>{
     const err = new CustomError(`Cant't find ${req.originalUrl} on the server!`, 404)
     next(err)
 })
 
+// Middleware Error
 app.use(ErrorController)
 
 app.listen(PORT, ()=> console.log(`Server is running on PORT http://localhost:${PORT}`))
