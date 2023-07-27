631253e4e1b8b7834c37cfced3df23a9dc69a398
tnnz20 <kaitora200@gmail.com>
2023-07-27 17:23:24 +0800

    feat: change name func and update params

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,17 +1,16 @@
 import * as ProductServices from './../services/ProductService.js'
 
-export async function createProduct (req, res, next){
+export async function addProduct (req, res, next){
     try {
-        const {title, price, url} = req.body
-
-        console.log(url)
-        const newProduct = await ProductServices.createProduct(
+        const {title, price, urlProduct, urlThumb} = req.body
+        const newProduct = await ProductServices.addProduct(
             title,
             price,
-            url
+            urlProduct,
+            urlThumb
         )
         res.status(201).json({
-            message: 'Product has benn successfully created...',
+            message: 'Product was added successfully...',
             data: newProduct
         });
     } catch (error) {
