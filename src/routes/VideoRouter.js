ac097713d3faf34f221c9d8169b58212e10c123c
tnnz20 <kaitora200@gmail.com>
2023-07-27 20:27:32 +0800

    feat: add route get by id

--- src/routes/VideoRouter.js
+++ src/routes/VideoRouter.js
@@ -3,7 +3,11 @@ import * as VideoControllers from './../controllers/VideoController.js'
 import { Router } from "express";
 const VideoRouter = Router()
 
-VideoRouter.route('/').post(VideoControllers.addVideo)
+VideoRouter.route('/')
+    .post(VideoControllers.addVideo)
+    .get(VideoControllers.getVideos)
+
+VideoRouter.route('/:videoId').get(VideoControllers.getVideoById)
 
 
 export default VideoRouter
