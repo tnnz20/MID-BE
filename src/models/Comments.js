b48e9323e28487bb3a682ec23f0a9b7654d8111b
tnnz20 <kaitora200@gmail.com>
2023-07-28 01:24:06 +0800

    feat: add comment Schema

--- src/models/Comments.js
+++ src/models/Comments.js
@@ -2,6 +2,25 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema
 
-const Comment = new Schema({
-    
-})
+const commentSchema = new Schema({
+        user: {
+            type:String,
+            required: [true, 'user not defined']
+        },
+        comment: {
+            type:String,
+            required: [true, 'comment not defined']
+        },
+        videoId: {
+            type: mongoose.ObjectId, 
+            ref: "Video",
+            required: [true, 'videoId not defined']
+        }
+    },
+    {timestamps:true}
+)
+
+
+const Comment = mongoose.model('Comment', commentSchema)
+
+export default Comment
