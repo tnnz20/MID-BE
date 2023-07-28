c31f576f775400fcb600d90b6ad12ae8df509ef5
tnnz20 <kaitora200@gmail.com>
2023-07-28 19:20:29 +0800

    feat: add validation and delete comment

--- src/services/CommentService.js
+++ src/services/CommentService.js
@@ -1,16 +1,32 @@
 import * as CommentRepositories from '../repositories/CommentRepository.js'
+import * as VideoServices from '../services/VideoService.js'
 import CustomError from '../utils/CustomError.js'
 
-export function postComment(user, comment, videoId){
-    if (!user || !comment || !videoId){
+export function postComment(username, comment, videoId){
+    if (!username || !comment || !videoId){
         throw new CustomError("There is empty data...", 400)
     }
-    return CommentRepositories.postComment(user, comment, videoId)
+    VideoServices.checkVideoId(videoId)
+    return CommentRepositories.postComment(username, comment, videoId)
 }
 export function getComments(){
     return CommentRepositories.getComments()
 }
 
 export function getCommentByVideoId(videoId){
+    VideoServices.checkVideoId(videoId)
     return CommentRepositories.getCommentByVideoId(videoId)
+}
+
+export function deleteCommentById(commentId){
+    if(commentId.length < 24){
+        throw new CustomError('Invalid Comment Id...', 400)
+    }
+    return CommentRepositories.deleteCommentById(commentId)
+}
+
+export function checkComment(Comment){
+    if(!Comment){
+        throw new CustomError('Comment not found...', 404)
+    }
 }
