ede4625166e1a8148df4141969f2e58cd09cb38b
tnnz20 <kaitora200@gmail.com>
2023-07-28 19:19:37 +0800

    feat: add validation and delete comment by id

--- src/controllers/CommentController.js
+++ src/controllers/CommentController.js
@@ -1,12 +1,12 @@
 import * as CommentsServices from "../services/CommentService.js"
-
+import CustomError from '../utils/CustomError.js';
 
 export async function postComment(req, res, next){
     try {
-        const { user, comment } = req.body
+        const { username, comment } = req.body
         const videoId = req.params.videoId
         const newComment = await CommentsServices.postComment(
-            user, 
+            username, 
             comment, 
             videoId
         )
@@ -22,6 +22,9 @@ export async function postComment(req, res, next){
 export async function getComments(req, res, next){
     try {
         const Comments = await CommentsServices.getComments()
+        if(Comments.length === 0){
+            throw new CustomError('Collection Comment still empty...', 404)
+        }
         res.status(200).json({
             message: 'Comments was retrieve successfully...',
             count: Comments.length,
@@ -36,11 +39,29 @@ export async function getCommentByVideoId(req, res, next){
     try {
         const videoId = req.params.videoId
         const Comment = await CommentsServices.getCommentByVideoId(videoId)
+        CommentsServices.checkComment(Comment)
+        if (Comment.length === 0){
+            throw new CustomError('Comment not found...', 404)
+        }
         res.status(200).json({
             message: 'Comments was retrieve successfully...',
             count: Comment.length,
             data: Comment
         })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function deleteCommentById(req, res, next){
+    try {
+        const commentId = req.query.commentId
+        const Comment = await CommentsServices.deleteCommentById(commentId)
+        CommentsServices.checkComment(Comment)
+        res.status(202).json({
+            message: 'Comment was deleted successfully...',
+            data: Comment
+        });
     } catch (error) {
         next(error)
     }
