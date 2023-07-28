914d90df1d4f193d5a7d48bf4eec6494631ecaa2
tnnz20 <kaitora200@gmail.com>
2023-07-28 16:22:43 +0800

    feat: add validation and delete video controller

--- src/controllers/VideoController.js
+++ src/controllers/VideoController.js
@@ -1,4 +1,5 @@
 import * as VideoServices from './../services/VideoService.js'
+import CustomError from '../utils/CustomError.js';
 
 export async function addVideo(req, res, next){
     try {
@@ -19,21 +20,29 @@ export async function addVideo(req, res, next){
 
 export async function getVideos(req, res, next){
     try {
-        const searchName = req.query.search
-        if (searchName){
-            const searchVideo = await VideoServices.searchVideoByName(searchName)
+        const textSearch = req.query.search
+
+        if (textSearch){
+            const searchVideo = await VideoServices.searchVideoByName(textSearch)
+            if (searchVideo.length === 0){
+                throw new CustomError(`Video title with ${textSearch} not found...`, 404)
+            }
             res.status(200).json({
                 message: 'Videos was retrieve successfully...',
                 count: searchVideo.length,
                 data: searchVideo
             });
+        }else{
+            const Videos = await VideoServices.getVideos()
+            if (Videos.length === 0){
+                throw new CustomError('Collection Videos still empty...', 404)
+            }
+            res.status(200).json({
+                message: 'Videos was retrieve successfully...',
+                count: Videos.length,
+                data: Videos
+            });
         }
-        const Videos = await VideoServices.getVideos()
-        res.status(200).json({
-            message: 'Videos was retrieve successfully...',
-            count: Videos.length,
-            data: Videos
-        });
     } catch (error) {
         next(error)
     }
@@ -43,8 +52,23 @@ export async function getVideoById(req, res, next){
     try {
         const videoId = req.params.videoId
         const Video = await VideoServices.getVideoById(videoId)
+        VideoServices.checkVideo(Video)
         res.status(200).json({
             message: 'Video was retrieve successfully...',
+            data: Video
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
+export async function deleteVideo(req, res, next){
+    try {
+        const videoId = req.query.videoId
+        const Video = await VideoServices.deleteVideoById(videoId)
+        VideoServices.checkVideo(Video)
+        res.status(202).json({
+            message: 'Video was deleted successfully...',
             data: Video
         });
     } catch (error) {
