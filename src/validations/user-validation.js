ed5323781fe94787e379ec3a48591258246eff96
tnnz20 <kaitora200@gmail.com>
2023-08-19 00:22:48 +0800

    feat: add new validation

--- src/validations/user-validation.js
+++ src/validations/user-validation.js
@@ -1,10 +1,18 @@
 import Joi from 'joi';
 
-const registerUserValidation = Joi.object({
+export const registerUserValidation = Joi.object({
     username: Joi.string().alphanum().lowercase().min(6).max(12).required(),
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(8).required(),
 });
 
-export default { registerUserValidation };
+export const loginUserValidation = Joi.object({
+    username: Joi.string().alphanum().lowercase().min(6).max(12).required(),
+    password: Joi.string().min(8).required(),
+});
+
+export const getUserValidation = Joi.string()
+    .regex(/^[0-9a-fA-F]{24}$/)
+    .min(24)
+    .required();
