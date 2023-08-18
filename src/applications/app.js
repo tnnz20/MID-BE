7df3b26709704cacc6fbb7726e0292ac52539e0d
tnnz20 <kaitora200@gmail.com>
2023-08-19 00:15:08 +0800

    feat: add new route and cookie-parser

--- src/applications/app.js
+++ src/applications/app.js
@@ -2,13 +2,17 @@ import express from 'express';
 import { errorMiddleware } from '../middleware/error-middleware.js';
 import { notFoundMiddleware } from '../middleware/404-middleware.js';
 import { publicRouter } from '../routes/public-api.js';
+import { userRouter } from '../routes/api.js';
+import cookieParser from 'cookie-parser';
 
 export const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 // Public routes
 app.use('/api/v2/', publicRouter);
+app.use('/api/v2/', userRouter);
 
 // Not found middleware
 app.use(notFoundMiddleware);
