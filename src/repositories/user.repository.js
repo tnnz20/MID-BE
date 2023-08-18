b9c2c92448680fd24ce4ae6af5310ab7c059a9fe
tnnz20 <kaitora200@gmail.com>
2023-08-19 00:20:06 +0800

    feat : add new user repository

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,9 +1,21 @@
 import User from '../models/users.model.js';
 
-export function createUser({ username, name, email, password }) {
-    return User.create({ username, name, email, password });
+export function createUser(request) {
+    return User.create(request);
+}
+
+export function findUser(request) {
+    return User.findOne({ username: request.username });
+}
+
+export function findUserById(request) {
+    return User.findById(request, { password: 0 });
 }
 
 export function countUser(username) {
     return User.countDocuments({ username: username });
+}
+
+export function countEmail(email) {
+    return User.countDocuments({ email: email });
 }
