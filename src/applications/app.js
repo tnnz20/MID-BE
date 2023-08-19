977250dbd7888c39dd060e2036766cb037d48ce1
tnnz20 <kaitora200@gmail.com>
2023-08-19 14:24:51 +0800

    fix: cors

--- src/applications/app.js
+++ src/applications/app.js
@@ -4,8 +4,16 @@ import { notFoundMiddleware } from '../middleware/404-middleware.js';
 import { publicRouter } from '../routes/public-api.js';
 import { userRouter } from '../routes/api.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 export const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:' + process.env.ALLOWED_CORS,
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 import 'dotenv/config';
 import connectDB from './applications/config/db.config.js';
 import { app } from './applications/app.js';
-import cors from 'cors';
 
 const PORT = process.env.PORT || 3000;
-
-const corsOptions = {
-    origin: `http://localhost:${process.env.ALLOWED_CORS}}`,
-    optionsSuccessStatus: 200,
-};
 
 connectDB()
     .then(() => {
         console.log('Connected to MongoDB...');
-        app.use(cors(corsOptions));
+
         app.listen(PORT, () =>
             console.log(`Server is running on PORT http://localhost:${PORT}`)
         );
